c8f9d2dfd53f22bbc2c89202a3a67c15a221cf67
Khent Lloyd Cases <khentlloyd3@gmail.com>
2025-01-28 20:56:22 +0800

    bg

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import { NavBar } from './components/NavBar';
 import { Banner } from './components/Banner';
@@ -6,17 +5,18 @@ import { Skills } from './components/Skills';
 import { TechStack } from './components/TechStack';
 import { Projects } from './components/Projects';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import ParticlesBackground from './components/ParticlesBackground';
 
 function App() {
   return (
-
-   <div className="App">
-    <NavBar />
-    <Banner />
-    <Skills />
-    <TechStack />
-    <Projects />
-   </div>
+    <div className="App">
+      <ParticlesBackground />
+      <NavBar />
+      <Banner />
+      <Skills />
+      <TechStack />
+      <Projects />
+    </div>
   );
 }
 
